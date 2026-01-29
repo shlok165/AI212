9d0a4281514cb2b1cacfe5b8d51ec26d8ea460d7
shlok <shlokvaidya11@gmail.com>
2026-01-29 22:45:45 +0530

    added insline styling

--- assignment3/2024aib1016_assignment_3/src/components/Student.jsx
+++ assignment3/2024aib1016_assignment_3/src/components/Student.jsx
@@ -1,7 +1,14 @@
 function Student(props) {
   return (
-    <div className="card">
-      <h2>Student Details</h2>
+    <div
+      className="card"
+      style={{
+        border: "2px solid #007bff",
+        backgroundColor: "#eaf3ff"
+      }}
+    >
+      <h2 style={{ color: "#007bff" }}>Student Details</h2>
+
       <p>Name: {props.name}</p>
       <p>Roll No: {props.rollNumber}</p>
       <p>Branch: {props.branch}</p>
